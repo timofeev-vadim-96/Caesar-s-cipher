9e711132f9b7c0bf7689b4f48375f5b1e13b02c2
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-07 21:23:41 +0300

    caesar rus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая переводит введенный текст в код цезаря, смещая буквы на n значений относительно алфавита
+char [] rusAlphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к',
+                       'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+                       'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+// string usAlphabet = "abcdefghijklmnopqrstuvwxyz";
+System.Console.WriteLine("Введите текст, который желаете зашифровать");
+string userText = Convert.ToString(Console.ReadLine());
+int n = 3;
+char [] resultText = new char [userText.Length];
+void CaesarCode (string inputText, char [] inputLanguage)
+{
+    for (int i = 0; i < inputText.Length; i++)
+    {
+        for (int j = 0; j < inputLanguage.Length; j++)
+        {
+            if (inputText[i] == inputLanguage[j]) resultText[i] = inputLanguage[j+n];
+        }
+    }
+}
+CaesarCode (userText, rusAlphabet);
+System.Console.WriteLine(resultText);
