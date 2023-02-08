8ea12c629ae0f5ca4be6f951715c8a6f7773cd07
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-08 22:15:09 +0300

    new save

--- Program.cs
+++ Program.cs
@@ -1,34 +1,50 @@
-﻿char [] rusAlphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к',
+﻿char[] rusAlphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к',
                        'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
                        'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В',
                        'Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р',
                        'С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я',
                       };
-// string usAlphabet = "abcdefghijklmnopqrstuvwxyz";
 System.Console.WriteLine("Введите текст, который желаете зашифровать");
-string userText = Convert.ToString(Console.ReadLine());
-int n = 3;
-char [] resultText = new char [userText.Length];
-void CaesarCode (string inputText, char [] inputLanguage)
+string userText = Console.ReadLine();
+string[] textArray = userText.Split(" ") // массив для определения длины слова
+                    .ToArray();
+int currentWordIndex = 0;
+int n = textArray[0].Length;
+char[] resultText = new char[userText.Length];
+char [] CopyTextInArray (char [] inputText, string InputUserText)
+{
+    for (int i = 0; i < resultText.Length; i++)
+    {
+        resultText[i] = InputUserText[i];
+    }
+    return inputText;
+}
+CopyTextInArray(resultText, userText);
+void CaesarCode(string inputText, char[] inputLanguage)
 {
     for (int i = 0; i < inputText.Length; i++)
     {
         int dif = 0;
-        int indexInLang = 0;
+        if (inputText[i] == ' ')
+        {
+            currentWordIndex++;
+            n = textArray[currentWordIndex].Length;
+        } 
         for (int j = 0; j < inputLanguage.Length; j++)
         {
-            if (inputText[i] == inputLanguage[j]) 
+            if (inputText[i] == inputLanguage[j])
             {
-                indexInLang = j; 
-                if (j+n > inputLanguage.Length/2)
+                if (j + n > inputLanguage.Length)
                 {
-                    dif = (j+n) - inputLanguage.Length/2;
+                    dif = (j + n) - inputLanguage.Length / 2;
                     resultText[i] = inputLanguage[dif];
+                    continue;
                 }
-                else resultText[i] = inputLanguage[j+n];
+                else resultText[i] = inputLanguage[j + n]; continue;
             }
         }
+
     }
 }
-CaesarCode (userText, rusAlphabet);
+CaesarCode(userText, rusAlphabet);
 System.Console.WriteLine(resultText);
